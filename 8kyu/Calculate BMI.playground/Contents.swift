ff2a98d7844079e56fbd3ddf2bbe3b20518795ff
Joao Ponte <joao_ponte@msn.com>
2023-07-05 17:18:40 +0100

    bool and BMI KATA

---
+++ 8kyu/Calculate BMI.playground/Contents.swift
@@ -0,0 +1,28 @@
+//Write function bmi that calculates body mass index (bmi = weight / height2).
+//
+//if bmi <= 18.5 return "Underweight"
+//
+//if bmi <= 25.0 return "Normal"
+//
+//if bmi <= 30.0 return "Overweight"
+//
+//if bmi > 30 return "Obese"
+
+import UIKit
+
+func bmi(_ weight: Int, _ height: Double) -> String {
+    var bmi = (Double(weight) / (height * height))
+    switch bmi {
+    case 0...18.5:
+        return "Underweight"
+    case 18.5...25.0:
+        return "Normal"
+    case 25.0...30.0:
+        return "Overweight"
+    case _ where bmi > 30:
+        return "Obese"
+    default:
+        return "Invalid BMI"
+    }
+    
+}
